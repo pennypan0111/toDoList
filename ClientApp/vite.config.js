1b9614cff9400668b2c5877c58a46bcb9535bc8b
pennypan0111 <minipan0111@gmail.com>
2024-10-18 15:25:39 +0800

    add PWA plugin and set PWA config

--- ClientApp/vite.config.js
+++ ClientApp/vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import eslintPlugin from 'vite-plugin-eslint'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,6 +13,51 @@ export default defineConfig({
     eslintPlugin({
       cache: false,
       include: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.jsx', 'src/**/*.cjs', 'src/**/*.mjs']
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'todoList',
+        short_name: 'todoList',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'fullscreen',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/appIcon.jpg',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /someInterface/i, // 接口緩存
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'interface-cache'
+            }
+          },
+          {
+            urlPattern: /(.*?)\.(js|css|ts)/, // 靜態資源緩存
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'js-css-cache'
+            }
+          },
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 圖片緩存
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache'
+            }
+          }
+        ]
+      }
     })
   ],
   resolve: {
